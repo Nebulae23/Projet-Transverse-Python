07305b63642d5175e52c970ae78d1529bd733e57
Neb <a.bonneauguiraud@gmail.com>
2025-01-21 14:01:08 +0100

    yes

---
+++ lib/utilities.py
@@ -0,0 +1,11 @@
+import pygame
+import os
+
+
+
+def load_config(): #load config file before starting the game
+    f= open("config.txt",'a')
+    dict=f.read
+    
+
+
--- main.py
+++ main.py
@@ -1,6 +1,38 @@
 import pygame
+import os
+
+
+import lib.utilities
+
+
+#init 
+pygame.init()
+screen = pygame.display.set_mode(0,0)
+clock = pygame.time.Clock()
+running = True
 
 
 
 
 
+
+
+#main function
+def main():
+    config=load_config() #load config
+            
+         
+    
+    pygame.quit()
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__' :
+    main()
