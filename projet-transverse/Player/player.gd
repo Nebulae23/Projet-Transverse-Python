5f8e4e3e4dd0197f9ac10514645d04aab95a2c96
Neb <a.bonneauguiraud@gmail.com>
2025-01-22 20:56:01 +0100

    3D working
    Issue with orientation and mooving

--- projet-transverse/Player/player.gd
+++ projet-transverse/Player/player.gd
@@ -33,5 +33,6 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 		anim.play("Idle_down")
-
+	
+	
 	move_and_slide()
