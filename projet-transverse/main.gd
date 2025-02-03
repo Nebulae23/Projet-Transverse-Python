b76e95d2b3998e798bbceed8bee5e4c35be8f92e
Neb <a.bonneauguiraud@gmail.com>
2025-02-03 10:47:47 +0100

    Update

--- projet-transverse/main.gd
+++ projet-transverse/main.gd
@@ -1,6 +1,6 @@
-extends Node2D
+extends Control
 
-
+@onready var level=preload("res://world.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -9,7 +9,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 
-func _on_button_2_pressed() -> void:
+func _on_quit_pressed() -> void:
 	get_tree().quit
-func _on_continue_game_pressed() -> void:
+	
+func _on_options_pressed() -> void:
+	get_tree().change_scene_to_file("res://options.tscn")
+func _on_new_game_pressed() -> void:
 	get_tree().change_scene_to_file("res://world.tscn")# Replace with function body.
