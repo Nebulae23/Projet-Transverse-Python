d90a285e066b25e706aee7f0c1e17073e99fc81c
Neb <a.bonneauguiraud@gmail.com>
2025-02-10 14:26:44 +0100

    update mechanics

--- projet-transverse/scenes/player/Hitbox_component.gd
+++ projet-transverse/scenes/player/Hitbox_component.gd
@@ -1,5 +1,8 @@
 extends Area3D
 
+@onready var health_component= $Health_Component
+@onready var status_component= $Status_Component
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -10,6 +13,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	for body in get_overlapping_bodies():
 		if body.is_in_group("Damage_source"):
+			var damage : int = body.data["damage"] #Body ==> for referecing the body that has entered the area, data is the dictionnary
+			var status : String = body.data["status"]
+			var duration : int = body.data["duration"]
+			var level : int = body.data["level"]
+			var type : String = body.data["type"]
+			health_component.take_damage(damage)
+			status_component.apply_effect(status,duration,level,type)
+			
 			pass
 	
 	
