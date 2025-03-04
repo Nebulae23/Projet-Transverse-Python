162120bbf9ca2b94c771337d6a928e2c5849850f
Neb <a.bonneauguiraud@gmail.com>
2025-03-04 15:00:04 +0100

    yes

--- projet-transverse/Components/stats_component.gd
+++ projet-transverse/Components/stats_component.gd
@@ -11,6 +11,127 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _get_modifier(stats:Dictionary):
-	if stats["type"]=="str":
-		pass
+
+#stats are a dictionnary with the following keys:
+#"type": the type of the stat
+#"value": the value of the stat
+#"weight": the weight of the stat
+
+"""data
+character datat is arranged as seen :
+	{
+	level: int
+	current_weapon 
+		{
+		main_stat:str
+		weapon_damage_bonus:int
+		weapon_crit_chance:float
+		weapon_crit_damage_bonus:int
+		}
+
+	stats:
+		{
+		strenght:
+			{
+			value:int
+			weight:float
+			}
+		vitality:
+			{
+			value:int
+			weight:float
+			}
+		agility:
+			{
+			value:int
+			weight:float
+			}
+		intelligence:
+			{
+			value:int
+			weight:float
+			}
+		luck:
+			{
+			value:int
+			weight:float
+			}
+		}
+	}
+
+
+
+
+
+
+
+example"""
+
+
+
+func _get_modifiying_value(character_data:Dictionary):
+
+	var level = character_data["level"]
+	var weapon_stat =0
+	var weapon_damage_bonus =0
+	for value in character_data["current_weapon"].values():
+		weapon_stat = value["main_stat"]
+		weapon_damage_bonus = value["weapon_damage_bonus"] 
+
+
+	var strength = character_data["stats"]["strength"]["value"]
+	var strength_weight = character_data["stats"]["strength"]["weight"]
+
+	var vitality = character_data["stats"]["vitality"]["value"]
+	var vitality_weight = character_data["stats"]["vitality"]["weight"]
+
+	var intelligence = character_data["stats"]["intelligence"]["value"]
+	var intelligence_weight = character_data["stats"]["intelligence"]["weight"]
+
+	var agility = character_data["stats"]["agility"]["value"]
+	var agility_weight = character_data["stats"]["agility"]["weight"]
+
+	var luck = character_data["stats"]["luck"]["value"]
+	var luck_weight = character_data["stats"]["luck"]["weight"]
+
+	var highest = 0
+	for key in character_data["stats"] :
+
+		if highest < character_data["stats"][key]["value"]:
+			highest = character_data["stats"][key]["value"]
+			if key == "strenght" or key == "agility":
+				highest *=1.25
+	
+	
+	var luck_processed=luck*luck_weight+level
+
+	
+
+
+
+
+
+	
+	var crit_chance=luck+agility*agility_weight
+	var overflow_1=0
+	if crit_chance > 100:
+		overflow_1 += (crit_chance - 100)
+
+	var crit_damage=luck*(highest+level)
+
+	var attack_touching_chance=(highest+level)*luck
+
+	var overflow_2=0
+	if attack_touching_chance > 100:
+		overflow_2 += (attack_touching_chance - 100)
+
+		
+	var attack_damage_bonus=(highest*level+weapon_damage_bonus)+(overflow_2)
+
+
+
+
+	
+		
+		
+		
