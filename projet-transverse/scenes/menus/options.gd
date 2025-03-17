35617df18857c6d8959c3c2a7cadb1925f62747b
Marine-Be <marine.bennet@efrei.net>
2025-03-17 14:54:10 +0100

    uu

--- projet-transverse/scenes/menus/options.gd
+++ projet-transverse/scenes/menus/options.gd
@@ -1,4 +1,9 @@
 extends Control
 
+Updated upstream
+
+func _ready() -> void:
+	pass # Replace with function body.
+
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://main.tscn")
+	get_tree().change_scene_to_file("res://scenes/menus/main.tscn")
